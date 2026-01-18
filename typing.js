2d63a61c40375f7ad00f53bf4fd1f780567a9664
ShivanshCodexx <shubhamclothes7422@gmail.com>
2026-01-18 19:07:05 +0530

    Create typing.js

---
+++ typing.js
@@ -0,0 +1,26 @@
+const texts = [
+  "HACKER THEME PORTFOLIO",
+  "STUDENT • DEVELOPER",
+  "CYBER STYLE PROFILE"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1200);
+  } else {
+    setTimeout(type, 80);
+  }
+})();
