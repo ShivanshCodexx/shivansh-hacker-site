2e2bda7f59d042ceb082932cc465b80d260a9f97
ShivanshCodexx <shubhamclothes7422@gmail.com>
2026-01-18 20:54:43 +0530

    terminal.js

--- terminal.js
+++ terminal.js
@@ -1,18 +1,5 @@
-const text = [
-  "Accessing secure server...",
-  "Bypassing firewall...",
-  "Decrypting data packets...",
-  "Access Granted ✔"
-];
-
-let i = 0;
-const terminalText = document.getElementById("terminal-text");
-
-function typeLine() {
-  if (i < text.length) {
-    terminalText.innerHTML += text[i] + "\n";
-    i++;
-    setTimeout(typeLine, 700);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    console.log("Command executed");
   }
-}
-typeLine();
+});
