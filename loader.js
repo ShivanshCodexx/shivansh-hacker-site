71d40ff5cb10a6f6bb214e6fceec416c07a29614
ShivanshCodexx <shubhamclothes7422@gmail.com>
2026-01-18 20:15:39 +0530

    loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,19 @@
-setTimeout(() => {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("main").style.display = "block";
-}, 3500);
+document.addEventListener("DOMContentLoaded", () => {
+  const terminal = document.getElementById("terminal");
+  const loader = document.getElementById("loader");
+  const main = document.getElementById("main");
+
+  terminal.style.display = "block";
+  loader.style.display = "none";
+  main.style.display = "none";
+
+  setTimeout(() => {
+    terminal.style.display = "none";
+    loader.style.display = "flex";
+  }, 3000);
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    main.style.display = "block";
+  }, 5500);
+});
