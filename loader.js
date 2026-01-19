244bfa4421c0ca44394f09dc289f09bb8939488b
ShivanshCodexx <shubhamclothes7422@gmail.com>
2026-01-19 14:02:00 +0530

    Delete loader.js

--- loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.getElementById("loader").style.display = "none";
-    document.querySelector(".terminal").classList.remove("hidden");
-    window.startTyping();
-  }, 2500);
-});
